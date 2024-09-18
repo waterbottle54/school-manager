63dc40d6d678b387a1cb3edeb41519f10bc1e4ef
sungwon jo <waterbottle54@naver.com>
2024-09-18 17:53:55 +0900

    file upload almost done

--- ui/AddProblemFragment.py
+++ ui/AddProblemFragment.py
@@ -1,4 +1,8 @@
-from PyQt5.QtWidgets import (QVBoxLayout, QHBoxLayout, QComboBox, QLabel, QSpinBox, QPushButton)
+import sys
+import shutil
+import os
+from PyQt5.QtWidgets import (QVBoxLayout, QHBoxLayout, QFileDialog, QLabel, QSpinBox, QPushButton)
+from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 from ui.common.Fragment import *
 from ui.common.UiUtils import *
@@ -11,8 +15,6 @@ class AddProblemFragment(Fragment):
     view_model: AddProblemViewModel
     layout: QHBoxLayout
 
-    problem_header: ProblemHeader
-   
     def __init__(self, title, view_model):
         super().__init__(title)
 
@@ -24,8 +26,23 @@ class AddProblemFragment(Fragment):
         self.setup_picture_section()
         self.setup_form_section()
 
+        self.view_model.full_title.observe(self.update_title)
+        self.view_model.image_path_1.observe(lambda image_path: self.update_picture(image_path, self.label_picture_1))
+        self.view_model.image_path_2.observe(lambda image_path: self.update_picture(image_path, self.label_picture_2))
+
         self.view_model.event.connect(self.on_event)
 
+    def update_title(self, title):
+        self.title = title if title is not None else ""
+
+    def update_picture(self, image_path, label):
+        label.clear()
+        if image_path is not None:
+            pixmap = QPixmap(image_path)
+            if pixmap is not None:
+                scaled = pixmap.scaled(label.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+                label.setPixmap(scaled)
+      
     def setup_picture_section(self):
         self.layout_picture = QHBoxLayout()
         self.layout.addLayout(self.layout_picture)
@@ -43,11 +60,11 @@ class AddProblemFragment(Fragment):
 
         self.button_upload_picture_1 = QPushButton('사진1 업로드')
         self.button_upload_picture_1.setObjectName('modify')
-        self.button_upload_picture_1.clicked.connect(self.view_model.on_upload_picture_1_click)
+        self.button_upload_picture_1.clicked.connect(self.view_model.on_select_picture_1_click)
 
         self.button_upload_picture_2 = QPushButton('사진2 업로드')
         self.button_upload_picture_2.setObjectName('modify')
-        self.button_upload_picture_2.clicked.connect(self.view_model.on_upload_picture_2_click)
+        self.button_upload_picture_2.clicked.connect(self.view_model.on_select_picture_2_click)
 
         self.layout_picture_1.addWidget(self.label_picture_1)
         self.layout_picture_1.addWidget(self.button_upload_picture_1)
@@ -69,9 +86,21 @@ class AddProblemFragment(Fragment):
         self.view_model.on_resume()
 
     def on_event(self, event):
-        pass
+        if isinstance(event, AddProblemViewModel.PromptImageFile):
+            self.select_image(event.sequence)
+        elif isinstance(event, AddProblemViewModel.MakeCopyImage):
+            self.make_copy_image(event.image_path, event.folder_name, event.file_name)
 
     def set_problem_header(self, header: ProblemHeader):
-        self.problem_header = header
-        if header is not None:
-            self.title = f"{header.book} {of_grade(header.grade)}-{header.chapter} prob.{header.title}"
+        self.view_model.on_problem_header_set(header)
+
+    def select_image(self, sequence):
+        image_path, _ = QFileDialog.getOpenFileName(self, "Open Image", "", "Image Files (*.png *.jpg *.bmp)")
+        if image_path:
+            self.view_model.on_image_result(sequence, image_path)
+
+    def make_copy_image(self, image_path, folder_name, file_name):
+        os.makedirs(folder_name, exist_ok=True)
+        base_name = os.path.basename(file_name)
+        destination_path = os.path.join(folder_name, base_name)
+        shutil.copy(image_path, destination_path)
--- ui/AddProblemViewModel.py
+++ ui/AddProblemViewModel.py
@@ -6,20 +6,64 @@ from data.Problem import *
 from data.BookRepository import *
 from data.ProblemRepository import *
 from data.ChapterRepository import *
+from common.StringRes import *
 
 class AddProblemViewModel(QObject):
 
     class Event:
         pass
 
+    class PromptImageFile(Event):
+        sequence: int
+        def __init__(self, sequence):
+            self.sequence = sequence
+
+    class MakeCopyImage(Event):
+        image_path: str
+        folder_name: str
+        file_name: str
+        def __init__(self, image_path, folder_name, file_name):
+            self.image_path = image_path
+            self.folder_name = folder_name
+            self.file_name = file_name
+
     event: pyqtSignal = pyqtSignal(Event)
+
+    problem_header: ProblemHeader
+
+    full_title: MutableLiveData
+
+    image_path_1: MutableLiveData
+    image_path_2: MutableLiveData
+
 
     def __init__(self):
         super().__init__()
+        self.full_title = MutableLiveData("")
+        self.image_path_1 = MutableLiveData(None)
+        self.image_path_2 = MutableLiveData(None)
     
     def on_resume(self):
         pass
 
-    def on_upload_picture_1_click(self):
-        pass
+    def on_problem_header_set(self, header: ProblemHeader):
+        self.problem_header = header
+        self.full_title.set_value(f"{header.book} {of_grade(header.grade)}-{header.chapter} prob.{header.title}")
+
+    def on_select_picture_1_click(self):
+        self.event.emit(AddProblemViewModel.PromptImageFile(0))
         
+    def on_select_picture_2_click(self):
+        self.event.emit(AddProblemViewModel.PromptImageFile(1))
+
+    def on_image_result(self, sequence, image_path):
+        if sequence == 0:
+            self.image_path_1.set_value(image_path)
+        elif sequence == 1:
+            self.image_path_2.set_value(image_path)
+
+        header = self.problem_header
+        folder_name = f'{header.book} {of_grade(header.grade)}-{header.chapter}'
+        file_name = f'{header.title}_{sequence}'
+
+        self.event.emit(AddProblemViewModel.MakeCopyImage(image_path, folder_name, file_name))
